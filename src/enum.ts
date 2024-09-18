886b0ddbacdc0c41dc5f0ff33cf583f8b29121ad
Manavsharma7017 <manavsharma7017@gmail.com>
2024-09-18 21:11:43 +0530

    enum

---
+++ src/enum.ts
@@ -0,0 +1,24 @@
+enum man{
+    up,
+    down,
+    left,
+    rigth
+}
+
+function dosomething(keypress:man):void{
+    console.log(keypress)
+}
+
+dosomething(man.rigth)
+enum man2{
+    up,
+    down,
+    left,
+    rigth
+}
+
+function dosomething2(keypress:man2):void{
+    console.log(keypress)
+}
+
+dosomething2(man2.rigth)
